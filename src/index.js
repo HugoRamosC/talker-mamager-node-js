d739dbb4c2914a30a745c59d280032810d8aefa6
HugoRamosC <hugor.castilho@gmail.com>
2023-02-07 13:38:56 -0300

    feat: add endpoint get /talker and try create server.js file

--- src/index.js
+++ src/index.js
@@ -1,16 +1,45 @@
 const express = require('express');
+const fs = require('fs').promises;
+const path = require('path');
 
 const app = express();
 app.use(express.json());
 
+const talkerPath = path.resolve(__dirname, './talker.json');
+
 const HTTP_OK_STATUS = 200;
+// const HTTP_CREATED_STATUS = 201;
+// const HTTP_NOT_FOUND_STATUS = 404;
+// const HTTP_INTERNAL_SERVER_ERROR_STATUS = 500;
+
 const PORT = '3000';
+
+app.listen(PORT, () => {
+  console.log('Online');
+});
+
+const readFile = async () => {
+  try {
+    const data = await fs.readFile(talkerPath);
+    return JSON.parse(data);
+  } catch (error) {
+    console.error(`Arquivo não pôde ser lido ${error}`);
+  }
+};
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
-app.listen(PORT, () => {
-  console.log('Online');
+app.get('/talker', async (_req, res) => {
+  try {
+    const talkers = await readFile();
+    if (!talkers) return res.status(404).send([]);
+    return res.status(HTTP_OK_STATUS).json(talkers);
+  } catch (err) {
+    return res.status(500).json({ message: `Internar error ${err}` });
+  }
 });
+
+module.exports = app;
